59a953da0c5ff0042055de7ab0d4809f13718c1f
nvd <navidsoleymani@ymail.com>
2022-09-04 12:18:32 +0430

    redesigned

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     license='BEENSI',
     author='beensi.com dev group',
     # author_email='email@example.com',
-    # packages=find_packages('src'),
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/beensi-packages/messages.git',
     # keywords='',
